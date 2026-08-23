456207bdba9e6edbb364bc06503f806059fd8f50
Dirk ten Have <47526227+WarlockGames12@users.noreply.github.com>
2026-08-23 16:15:20 +0200

    Finished camera feature

--- Assets/Scripts/PlayerScript/CameraMovementScript.cs
+++ Assets/Scripts/PlayerScript/CameraMovementScript.cs
@@ -5,9 +5,17 @@ public class CameraMovementScript : MonoBehaviour
 
     [Header("Camera Settings:")]
     [SerializeField] private Transform target;
-    [SerializeField, Range(0, 100)] private float speed;
+    [SerializeField, Range(0, 1000)] private float speed;
     [SerializeField] private Vector3 camOffset;
 
+    // private variables
+    private Vector3 cam_velocity;
+
     // Update is called once per frame
-    private void LateUpdate() => transform.position = Vector3.Lerp(transform.position, target.position + camOffset, speed * Time.deltaTime);
+    private void LateUpdate()
+    {
+        // Smoothly move to player
+        var cam_pos = target.position + camOffset;
+        transform.position = Vector3.SmoothDamp(transform.position, cam_pos, ref cam_velocity, speed);
+    }
 }
