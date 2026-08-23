e820e4916d857c3be4ca9e20dcc1d496537f44aa
Dirk ten Have <47526227+WarlockGames12@users.noreply.github.com>
2026-08-23 12:58:05 +0200

    The Beginning of it all

---
+++ Assets/Scripts/PlayerScript/PlayerScript.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+
+    [Header("Player Settings:")]
+    [SerializeField, Range(0, 100)] private float playerSpeed; 
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
